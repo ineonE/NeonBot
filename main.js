509328e939d9ff4d14f889876f79498e46bd0f46
TheMainGeek <49459814+TheMainGeek@users.noreply.github.com>
2021-04-05 11:26:54 -0400

    Update main.js
    Remove some unnecessary whitespace

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-
 const Discord = require('discord.js');
 const mongoose = require('mongoose')
 require('dotenv').config();
 const prefix = process.env.PREFIX
 const client = new Discord.Client();
 
-
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
-
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord)
@@ -26,30 +23,18 @@ mongoose.connect(process.env.MONGOPATH_SRV,  {
     console.log(err)
 })
 
-
 client.on('message', async message => {
-
-
 if (message.mentions.size < 1 || !message.guild || message.author.bot) return;
-      const db = require('./models/afkSchema')
-      const user = message.mentions.users.first()
-      if (!user) return
+      const db = require('./models/afkSchema');
+      const user = message.mentions.users.first();
+      if (!user) return;
       await db.findOne({ guildId: message.guild.id, userId: user.id}, async (err, res) => {
-          if (err || !res) return
+          if (err || !res) return;
           if (res) {
-              message.delete()
-              return message.reply(`Hey! ${user.username} is marked as AFK!`)
-           
-         
-
-    
-
-
+              message.delete();
+              return message.reply(`Hey! ${user.username} is marked as AFK!`);
             }
-        })
-})
+        });
+});
 
-
-client.login(process.env.DISCORD_TOKEN)
-      
-
+client.login(process.env.DISCORD_TOKEN);
